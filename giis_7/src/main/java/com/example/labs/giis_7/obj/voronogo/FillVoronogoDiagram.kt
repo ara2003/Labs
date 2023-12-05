9568d56579c615d2a9e57a36a3841722f25a2934
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-12-06 00:22:08 +0300

    delete unused code

--- giis_7/src/main/java/com/example/labs/giis_7/obj/voronogo/FillVoronogoDiagram.kt
+++ giis_7/src/main/java/com/example/labs/giis_7/obj/voronogo/FillVoronogoDiagram.kt
@@ -6,33 +6,20 @@ import com.example.labs.giis_7.pixel.PixelDrawer
 import java.awt.Color
 import java.util.concurrent.ThreadLocalRandom
 
-private val PixelDrawer.widthScale
-	get() = when(this) {
-		is WidthSplit -> 2 * origin.width
-		is HeightSplit -> origin.width
-		else -> 1
-	}
-private val PixelDrawer.heightScale
-	get() = when(this) {
-		is WidthSplit -> origin.width
-		is HeightSplit -> 2 * origin.width
-		else -> 1
-	}
-
 object FillVoronogoDiagram : VoronogoDiagram {
 
 	val colors = mutableMapOf<Int, Color>()
 
 	override fun draw(drawer: PixelDrawer, points: Iterable<Point>) {
-		val points = points.map { (x, y) -> Point(x / drawer.widthScale, y / drawer.heightScale) }
+		val points = points.toList()
 		for(x in 0 .. drawer.width)
 			for(y in 0 .. drawer.height) {
 				val p = Point(x, y)
 				val c = points.minIndexBy { distanceSqr(p, it) }
 				val color = colors.getOrPut(c) {
-					val r = ThreadLocalRandom.current().nextInt(256)
-					val g = ThreadLocalRandom.current().nextInt(256)
-					val b = ThreadLocalRandom.current().nextInt(256)
+					val r = ThreadLocalRandom.current().nextInt(100, 256)
+					val g = ThreadLocalRandom.current().nextInt(100, 256)
+					val b = ThreadLocalRandom.current().nextInt(100, 256)
 					Color(r, g, b)
 				}
 				drawer.drawPixel(x, y, color)
